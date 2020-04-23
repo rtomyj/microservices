a5ca4487eb8b08a5e5ad50ca187ff3710f741c1f
Javi Gomez <rtomyj@gmail.com>
2020-04-22 20:26:10 -0500

    project structure changes

--- cloud-server/src/main/java/com/rtomyj/udemy/microservices/cloudsever/CloudSeverApplication.java
+++ cloud-server/src/main/java/com/rtomyj/udemy/microservices/cloudsever/CloudSeverApplication.java
@@ -2,8 +2,10 @@ package com.rtomyj.udemy.microservices.cloudsever;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudSeverApplication {
 
 	public static void main(String[] args) {
