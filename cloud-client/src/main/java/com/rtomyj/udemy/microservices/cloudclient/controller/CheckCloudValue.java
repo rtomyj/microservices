8a11bf30ad6b5370ccf0df527bb9bc8620e10a19
Javi Gomez <rtomyj@gmail.com>
2020-04-23 19:34:49 -0500

    Adding new config files

--- cloud-client/src/main/java/com/rtomyj/udemy/microservices/cloudclient/controller/CheckCloudValue.java
+++ cloud-client/src/main/java/com/rtomyj/udemy/microservices/cloudclient/controller/CheckCloudValue.java
@@ -1,8 +1,6 @@
 package com.rtomyj.udemy.microservices.cloudclient.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CheckCloudValue
 {
-	@Autowired
-	private Environment environment;
-
 	@Value("${config.value}")
 	private String value;
+
 
 	@GetMapping("/{propName}")
 	public ResponseEntity<String> getProp()
 	{
-		System.out.println(environment.toString());
 		return ResponseEntity.ok(value);
 	}
 }
